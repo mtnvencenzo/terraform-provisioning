a001df56e89dbb75c79a9ee6bfbb736bdc95fae0
Ronaldo Vecchi <rvecchi@gmail.com>
2025-10-08 18:51:58 -0600

    refactor (terraform): Refactoring files and setup for primary tf init… (#5)
    * refactor (terraform): Refactoring files and setup for primary tf initialization
    
    * fixing fmt
    
    * renaming pipline

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,64 +18,15 @@ provider "azurerm" {
   features {}
 }
 
-# provider "azuredevops" {
-#   org_service_url       = var.devops_url
-#   personal_access_token = var.PAT
-# }
-
-# data "azuredevops_users" "user_rvecchi" {
-#   principal_name = "rvecchi@gmail.com"
-# }
-
 module "latest_project" {
-  source            = "./projects/latest"
-  workitem_template = var.workitem_template
-  version_control   = var.version_control
-  visibility        = var.visibility
-  //devops_url                = var.devops_url
-  azurerm_spn_tenantid      = var.azurerm_spn_tenantid
-  azurerm_subscription_id   = var.azurerm_subscription_id
-  azurerm_subscription_name = var.azurerm_subscription_name
-  global_environment        = var.global_environment
-  location                  = var.location
-  sub                       = var.sub
-  sequence                  = var.sequence
-  //code_reviewer_user        = tolist(data.azuredevops_users.user_rvecchi.users)[0]
+  source             = "./projects/latest"
+  global_environment = var.global_environment
+  location           = var.location
+  sub                = var.sub
+  sequence           = var.sequence
 
   providers = {
     azurerm     = azurerm,
     azuredevops = azuredevops
   }
 }
-
-# module "coldstorage_project" {
-#   source             = "./projects/coldstorage"
-#   workitem_template  = var.workitem_template
-#   version_control    = var.version_control
-#   visibility         = var.visibility
-#   devops_url         = var.devops_url
-#   sub                = var.sub
-#   sequence           = var.sequence
-#   code_reviewer_user = tolist(data.azuredevops_users.user_rvecchi.users)[0]
-
-#   providers = {
-#     azurerm     = azurerm,
-#     azuredevops = azuredevops
-#   }
-# }
-
-# module "codechallenge_project" {
-#   source             = "./projects/codechallenge"
-#   workitem_template  = var.workitem_template
-#   version_control    = var.version_control
-#   visibility         = var.visibility
-#   devops_url         = var.devops_url
-#   sub                = var.sub
-#   sequence           = var.sequence
-#   code_reviewer_user = tolist(data.azuredevops_users.user_rvecchi.users)[0]
-
-#   providers = {
-#     azurerm     = azurerm,
-#     azuredevops = azuredevops
-#   }
-# }
--- terraform/projects/latest/variables.tf
+++ terraform/projects/latest/variables.tf
@@ -1,43 +1,3 @@
-variable "PAT" {
-  type    = string
-  default = ""
-}
-
-variable "workitem_template" {
-  type    = string
-  default = "Scrum"
-}
-
-variable "version_control" {
-  type    = string
-  default = "Git"
-}
-
-variable "visibility" {
-  type    = string
-  default = "private"
-}
-
-# variable "devops_url" {
-#   type    = string
-#   default = "https://dev.azure.com/vecchi"
-# }
-
-variable "azurerm_spn_tenantid" {
-  type    = string
-  default = "abc490e5-395b-4db1-9d9f-18434a45263d"
-}
-
-variable "azurerm_subscription_id" {
-  type    = string
-  default = "1d9ecc00-242a-460d-8b08-b71db19f094e"
-}
-
-variable "azurerm_subscription_name" {
-  type    = string
-  default = "sub-vecchi-001"
-}
-
 variable "global_environment" {
   type        = string
   description = "Global environment name, e.g. 'dev' or 'stage' or 'prod'"
@@ -66,9 +26,3 @@ variable "sequence" {
   description = "The sequence number of the resource typically starting with 001"
   default     = "001"
 }
-
-# variable "code_reviewer_user" {
-#   type = object({
-#     id = string
-#   })
-# }
